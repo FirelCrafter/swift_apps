e9f83075625bdf91eb38aaa826f11e310f4342b3
FirelCrafter <64683920+FirelCrafter@users.noreply.github.com>
2021-06-25 13:53:04 +0500

    Add files via upload

---
+++ SwiftUI Animations/SpriteKit Animation/SpriteKit Animation/SpriteKit_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_AnimationApp.swift
+//  SpriteKit Animation
+//
+//  Created by Михаил Щербаков on 24/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
